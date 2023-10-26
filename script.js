d256de01c4d95002c864606d9ee4aad2bbca7ea5
karan <karanverma201411@gmail.com>
2023-10-26 12:00:54 +0530

    js changed

--- script.js
+++ script.js
@@ -28,61 +28,54 @@ const closeCoupon = () => {
 
 
 const changeMode = () => {
-    let myBody = document.body
+    const myBody = document.body
+    const myNavi = document.getElementsByClassName('navbar1')[0];
+    const footer = document.getElementsByClassName('footer')[0];
+    
     myBody.classList.toggle('myDark')
+    myNavi.classList.toggle('myDark')
+    footer.classList.toggle('myDark')
 }
 
 
 
 
-// Getting the user's temperature and location 
+// geolocation 
 
-// const findLocation=()=>{
-//     const temp = document.getElementById('temp')
+const findLocation = () => {
+    const temp = document.getElementById('temp')
 
-//     const success=(position)=>{
-//         console.log(position);
-//     }
+    const success = (position) => {
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
 
-//     const error=()=>{
-//         temp.textContent = 'Unable to get your location'
-//     }
+        const geoAPI = `https://api.openweathermap.org/data/2.5/forecast/daily?lat=${latitude}&lon=${longitude}&mode=json&units=metric&cnt=5&appid=fbf712a5a83d7305c3cda4ca8fe7ef29`
 
+        fetch(geoAPI, { method: 'GET' })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                let cityName = data.city.name;
+                let temperature = data.list[0].temp.day + '°C';
+                temp.textContent = `Your city, ${cityName}'s temperature is ${temperature}`
+            })
+    }
 
-//     navigator.geolocation.getCurrentPosition(success, error)
-// }
+    const error = () => {
+        temp.textContent = 'Unable to get your location'
+    }
 
-// document.getElementById('getTemp').addEventListener('click', findLocation)
+    navigator.geolocation.getCurrentPosition(success, error)
+}
 
-// const geolocation = () => {
-//     let x = document.getElementById('temp')
+document.getElementById('getTemp').addEventListener('click', findLocation)
 
-//     if (navigator.geolocation) {
-//         navigator.geolocation.getCurrentPosition(showPosition)
-//     }
-//     else {
-//         x.innerHTML = 'Geo location not supported on your browser!'
-//     }
-// }
+const visible = document.getElementById('newAlert')
+const getTemp = document.getElementById('getTemp')
 
-// const showPosition = (data) => {
-//     let lat = data.coords.latitude;
-//     let lon = data.coords.longitude;
+const makeVisible = () => {
+    visible.style.visibility = 'visible'
+    getTemp.style.visibility = 'hidden'
+}
 
-//     x.innerHTML = `Latitude is ${lat} and longitude is ${lon}`
-
-//     const url = `https://api.openweathermap.org/data/2.5/forecast/daily?lat=${lat}&lon=${lon}&mode=json&units=metric&cnt=5&appid=fbf712a5a83d7305c3cda4ca8fe7ef29`
-
-//     //api calling
-//     fetch(url, { method: 'GET' })
-
-//         // returns a promise 
-//         .then((res) => res.json())
-        
-//         // resolving the promise 
-//         .then((data) => {
-//             let cityName = data.city.name;
-//             let tempr = data.list[0].temp.day + '°C';
-//             x.innerHTML = `The weather of ${cityName} is ${tempr}`
-//         })
-// }
+document.getElementById('getTemp').addEventListener('click', makeVisible)
